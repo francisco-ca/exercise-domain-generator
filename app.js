b81bc13cd8d6bae0afbf159cfb9d7e39ca9d598b
francisco-ca <francisco4_1996@hotmail.com>
2020-09-11 20:04:27 +0000

    added more variables

---
+++ app.js
@@ -0,0 +1,9 @@
+  var pronoun = ['the','our','your','his','their'];
+  var adj = ['great', 'big','amazing','strong','weak','polite','inmortal','shy','pretencious','intrepid' ];
+  var noun = ['jogger','racoon','spiderman','tree','domain','cat','dog','spider','bird','potatoe','carrot'];
+  var ext= ['.com','.es','.co', '.cl','.br','.uk','.fr','.po','.ge','.us','.pe']
+prandom = pronoun[Math.floor(Math.random()*pronoun.length)]
+adjrandom = adj[Math.floor(Math.random()*adj.length)]
+nounrandom = noun[Math.floor(Math.random()*noun.length)]
+console.log(prandom)
+//   for (let i=0;i < pronoun.length);i++){console.log(pronoun[Math.floor(Math.random()*pronoun.length)]}
