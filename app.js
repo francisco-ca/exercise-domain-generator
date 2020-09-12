0c75b2cd85ae0f0fdf2773c2407ebf59d33bbbfe
francisco-ca <francisco4_1996@hotmail.com>
2020-09-12 16:34:40 +0000

    Ahora genera los nombres de dominios

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
   var pronoun = ['the','our','your','his','their'];
   var adj = ['great', 'big','amazing','strong','weak','polite','inmortal','shy','pretencious','intrepid' ];
   var noun = ['jogger','racoon','spiderman','tree','domain','cat','dog','spider','bird','potatoe','carrot'];
-  var ext= ['.com','.es','.co', '.cl','.br','.uk','.fr','.po','.ge','.us','.pe']
-prandom = pronoun[Math.floor(Math.random()*pronoun.length)]
-adjrandom = adj[Math.floor(Math.random()*adj.length)]
-nounrandom = noun[Math.floor(Math.random()*noun.length)]
-console.log(prandom)
-//   for (let i=0;i < pronoun.length);i++){console.log(pronoun[Math.floor(Math.random()*pronoun.length)]}
+  var ext= ['.com','.es','.co','.cl','.br','.uk','.fr','.po','.ge','.us','.pe']
+
+
+const randomdomain = () => {
+let domainname = "";
+for (let i=0; i< pronoun.length; i++){
+     
+    for (let j=0; j< adj.length; j++){
+     
+    for (let k=0; k< noun.length; k++){
+    
+    for (let l=0; l< ext.length; l++){
+   
+    domainname = pronoun[i] + adj[j]+ noun[k] + ext[l]
+
+console.log (domainname);}}}}
+
+
+;}
+
+randomdomain()
